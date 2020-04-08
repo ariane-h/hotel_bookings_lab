c7b2f3fd0e25702d0caa4de053cf5f99e2dc50be
Ariane Holford <aribomop@gmail.com>
2020-04-08 11:42:52 +0100

    connect to db and setup routes

--- server/server.js
+++ server/server.js
@@ -1,11 +1,22 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 const cors = require('cors');
+const MongoClient = require('mongodb').MongoClient;
+const createRouter = require('./helpers/create_router.js');
 
 app.use(cors());
+app.use(bodyParser.json());
 
-app.get('/api/bookings', (req, res) => {
-    res.json( { message: "Let's get the party started"} )
-})
+MongoClient.connect('mongodb://localhost:27017')
+  .then((client) => {
+    const db = client.db('hotel_bookings');
+    const bookingsCollection = db.collection('bookings');
+    const bookingsRouter = createRouter(bookingsCollection);
+    app.use('/api/bookings', bookingsRouter);
+  })
+  .catch(console.err);
 
-app.listen(3001)
+app.listen(3001, function () {
+  console.log(`Listening on port ${ this.address().port }`);
+});
